7514abe7d352d596e902ed7e39c83dec583bd5cb
yoneyama ryo <uryonym@gmail.com>
2025-08-27 23:02:23 +0900

    eslint設定を実施

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,75 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import unusedImports from 'eslint-plugin-unused-imports'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
-];
+  {
+    // eslint-plugin-importに関する設定
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always', // import groups 1行空ける
+          pathGroups: [
+            {
+              pattern: 'src/components/**',
+              group: 'internal',
+              position: 'before',
+            },
+            { pattern: 'src/lib/**', group: 'internal', position: 'before' },
+          ],
+        },
+      ],
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
+  },
+  {
+    // eslint-plugin-unused-importsに関する設定
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+  {
+    // その他設定
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+    languageOptions: {
+      globals: {
+        React: 'readonly',
+      },
+    },
+    rules: {
+      'react/jsx-boolean-value': 'error', // JSXの中でのbooleanの使用
+      'react/jsx-curly-brace-presence': 'error', // JSXの中での余分な{}の使用
+    },
+  },
+  eslintConfigPrettier, // Prettierとの競合防止
+]
 
-export default eslintConfig;
+export default eslintConfig
