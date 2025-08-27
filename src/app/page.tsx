d9f162eac6ba85966a57f5efece5cfa982274a40
yoneyama ryo <uryonym@gmail.com>
2025-08-27 23:29:47 +0900

    supabaseの基本設定とデータ取得処理を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { createClient } from '@/utils/supabase/server'
+
+export default async function Home() {
+  const supabase = await createClient()
+  const { data: categories } = await supabase.from('categories').select()
+
   return (
     <div>
       <p>ynym-kakeibo</p>
+      <ul>
+        {categories &&
+          categories.map((category) => (
+            <li key={category.id}>
+              {category.seq}:{category.name}
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
