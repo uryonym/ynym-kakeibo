ba9a25d899316ddc3edc6c3f85cdca60da8e2556
yoneyama ryo <uryonym@gmail.com>
2025-08-29 23:11:54 +0900

    eslintの設定にsrc/components/uiディレクトリを無視リストに追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,14 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      'src/components/ui/**',
+    ],
   },
   {
     // eslint-plugin-importに関する設定
